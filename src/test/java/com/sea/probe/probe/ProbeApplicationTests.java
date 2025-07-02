7b6685b965227b11eec7a60f63282420c0aebe4f
Jitendra Singh Kushwaha <jitendra@Jitendras-MacBook-Pro.local>
2025-07-02 17:19:07 +0530

    framework init

---
+++ src/test/java/com/sea/probe/probe/ProbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sea.probe.probe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
